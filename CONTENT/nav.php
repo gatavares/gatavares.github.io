e79bfe7d791ef9b5b6a5b04b3a62d64bc2c577b1
Guilherme Tavares <joseserafimtavares89@gmail.com>
2022-07-18 20:44:46 +0100

    navbar animated

--- CONTENT/nav.php
+++ CONTENT/nav.php
@@ -1,9 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-ghost">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">‹GAT/›</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon" fill="#000"></span>
+            <span class="toggler-icon top-bar"></span>
+            <span class="toggler-icon middle-bar"></span>
+            <span class="toggler-icon bottom-bar"></span>
         </button>
         <div class="collapse navbar-collapse me-auto" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
