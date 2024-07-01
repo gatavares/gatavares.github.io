59a905a16454b366aafde8a1886077eba216e8bc
Guilherme Tavares <joseserafimtavares89@gmail.com>
2024-07-01 04:07:28 +0100

    Correções
    Funcao novo() desativada;
    cursor no link atualzado

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,10 @@
 let idSaved = 1;
 
+const data = new Date();
+let ano = data.getFullYear();
+console.log(`&copy; ` + ano);
+document.getElementById("copy").innerHTML = `&copy; ` + ano;
+
 function menu(id) {
     const element = document.getElementById(id);
     if (id === idSaved) {
@@ -20,23 +25,21 @@ function menu(id) {
     }
 }
 
-function novo() {
-    const width = window.innerWidth
-    console.log(width)
+// function novo() {
+//     const width = window.innerWidth
+//     console.log(width)
 
-    if (width >= 1200) {
-        document.getElementById('novo2').className = "showned"
-        document.getElementById('novo').className = "hiden"
-        document.getElementById('novo3').className = "hiden"
-    }
-    else {
-        document.getElementById('novo').className = "showned"
-        document.getElementById('novo3').className = "showned"
-        document.getElementById('novo2').className = "hiden"
-    }
-}
+//     if (width >= 1200) {
+//         document.getElementById('novo').className = "showned"
+//         document.getElementById('novo2').className = "hiden"
+//     }
+//     else {
+//         document.getElementById('novo2').className = "showned"
+//         document.getElementById('novo').className = "hiden"
+//     }
+// }
 
-novo()
+// novo();
 
 function getPoint(id) {
     document.querySelector(id).scrollIntoView({
@@ -44,12 +47,8 @@ function getPoint(id) {
     });
 }
 
-const data = new Date();
-let ano = data.getFullYear();
-document.getElementById("copy").innerHTML = `&copy; ` + ano;
-
 
 function warning() {
     alert("Não foi permitido publicar este projeto! Sugiro visualizar outro projeto.");
-    getPoint("#PAP"); 
+    getPoint("#PAP");
 }
